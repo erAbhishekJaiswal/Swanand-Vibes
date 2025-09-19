8d8a844df4217bb6e4dd7bf229eebef81a6bf02a
Abhishek <er.abhishek72@gmail.com>
2025-09-19 22:50:46 +0530

    update the withdraw

--- src/pages/Admin/Wallet/WithdrawalRequests.jsx
+++ src/pages/Admin/Wallet/WithdrawalRequests.jsx
@@ -399,6 +399,14 @@ if (statusFilter !== 'all') {
     
     if (!request) return;
     
+    if(newStatus === "withdrawal-rejected"){
+      // API call to update status
+    await axios.put(`http://localhost:5000/api/user/wallet/${request._id}/reject-withdrawal`, {
+      walletId: request._id,
+      txnId: request.transactions[0]._id,
+      status: newStatus
+    });
+    }
     // API call to update status
     await axios.put(`http://localhost:5000/api/user/wallet/${request._id}/approve-withdrawal`, {
       walletId: request._id,
